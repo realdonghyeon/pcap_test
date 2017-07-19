6843a87284508d3860e1c2aab521c962f26a923d
donghyeon2 <tsvz1234@gmail.com>
2017-07-20 01:15:08 +0900

    make all parts function and fixing bug

--- pcap.c
+++ pcap.c
@@ -7,101 +7,60 @@
 #include <pcap/pcap.h>
 #include <string.h>
 
-struct mac_address {
-	u_int8_t shost[6];
-	u_int8_t dhost[6];
-}__attribute__ ((__packed__));
 
-struct ip_address {
-	u_int8_t sadd[4];
-	u_int8_t dadd[4];
-}__attribute__ ((__packed__));
-
-struct port {
-	uint16_t sport;
-	uint16_t dport;
-
-}__attribute__ ((__packed__));
+void mac_address(const unsigned char* pak);
 
 int main(int argc, char *argv[]){
-	pcap_t *handle;			
-	char *dev;			
-	char errbuf[PCAP_ERRBUF_SIZE];	
-	struct bpf_program fp;	
-	char filter_exp[] = "";
+
+	pcap_t *handle;
+	char *dev;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	struct bpf_program fp;
+	char filter_ex[] = "port 80";
 	bpf_u_int32 mask;
-	bpf_u_int32 net;	
+	bpf_u_int32 net;
 	struct pcap_pkthdr header;
 	const u_char *packet;
-	struct mac_address *mac;
-	struct ip_address *ip;
-	struct port *pt;
-	char *data;
 	int res;
-	/*char s_ip[6]*/
+
 	dev = pcap_lookupdev(errbuf);
-	
-	if (dev == NULL) {
-		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+	if(dev == NULL){
+		printf("Error %s \n", errbuf);
 		return(2);
 	}
-		
-	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
-		net = 0;
-		mask = 0;
+	if(pcap_lookupnet(dev, &net, &mask, errbuf) == -1){
+		printf("error device %s: %s \n",dev, errbuf);
+		net=0;
+		mask=0;
 	}
-		
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-	if (handle == NULL) {
-		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+	if(handle == NULL){
+		printf("%s : %s \n", dev, errbuf);
 		return(2);
 	}
-		
-	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+	if(pcap_compile(handle, &fp, filter_ex, 0, net) == -1){
+		printf("%s : %s \n", filter_ex, pcap_geterr(handle));
 		return(2);
 	}
-	if (pcap_setfilter(handle, &fp) == -1) {
-		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+	if(pcap_setfilter(handle, &fp) == -1){
+		printf("%s : %s\n", filter_ex, pcap_geterr(handle));
 		return(2);
 	}
-		
-	packet = pcap_next(handle, &header);
-		
-	mac = (struct mac_address*)packet;
-	pt = (struct port*)packet;
-	ip = (struct ip_address*)packet;
-	data = (char*)packet+34;
-	printf("SOURCE MAC %02x:%02x:%02x:%02x:%02x:%02x \n", 
-	mac->shost[0], mac->shost[1], mac->shost[2], mac->shost[3],
-	mac->shost[4], mac->shost[5]);
-		
-	printf("DESTINATION MAC %02x:%02x:%02x:%02x:%02x:%02x \n",
-	mac->dhost[0], mac->dhost[1], mac->dhost[2], mac->dhost[3],
-	mac->dhost[4], mac->dhost[5]);
-	
-	printf("SOURCE IP : %d.%d.%d.%d \n", ip->sadd[0], ip->sadd[1]
-	, ip->sadd[2], ip->sadd[3]);
-	printf("DESTINATION IP : %d.%d.%d.%d \n", ip->dadd[0], ip->dadd[1]
-	, ip->dadd[2], ip->dadd[3]);
-	printf("SOURCE PORT : %d \n", pt->sport);
-	printf("DESTINATION PORT : %d \n", pt->dport);
-	memcpy(data, packet, 1024);
-	/*for(int i=36; i<1024; i++){
-		printf("%02x ", data[i]);
-		if(i%16==0)
-			printf("\n");				//packet data print
+
+	while((res=pcap_next_ex(handle, &header, &packet))>=0){
+		if(res==0) 
+			continue;
+	mac_address(packet);
 	}
-	printf("\n");*/
-	
-	/*printf("DESTINATION IP : %s \n", inet_ntoa(ip->dadd));
-	printf("SOURCE PORT : %d \n", port->th_sport);*/
-	/*printf("DESTINATION PORT : %d \n",port->th_dport);*/
-	/*printk(KERN_DEBUG, "%pI4", &ip->saddr);*/
-	/*memcpy(s_ip, &ip->saddr, 16);*/
-	/*snprintf(s_ip, 16, "%pI4", &ip->saddr);*/
-	pcap_close(handle);
-	
-	return(0);
+	return (0);
+}
+
+
+void mac_address(const unsigned char* pak){
+	struct ether_header *ep;
+	ep = (struct ether_header*)pak;
+
+	printf("SROUCE : %02x:%02x:%02x:%02x:%02x:%02x",ep->ether_shost[0], ep->ether_shost[1], ep->ether_shost[2],
+			ep->ether_shost[3], ep->ether_shost[4], ep->ether_shost[5]);
+
 }
